0800c64d7a2e83d3de352b751abc18a21fe0cd76
Jacob Daughtry <akutozo@gmail.com>
2020-07-04 13:55:09 -0500

    Beautiful. Just beautiful.

--- assets/js/script.js
+++ assets/js/script.js
@@ -150,15 +150,21 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
     // console.log("activate", this);
   },
   deactivate: function(event){
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
     // console.log("deactivate", event.target);
   },
   over: function(event) {
+    $(event.target).addClass("dropover");
     // console.log("over", event.target);
   },
   out: function(event){
+    $(event.target).removeClass("dropover");
     // console.log("out", event.target);
   },
   update: function(event) {
@@ -219,9 +225,11 @@ $("#trash").droppable({
     console.log("drop");
   },
   over: function(event, ui) {
+    $(".bottom-trash").addClass("bottom-trash-active");
     console.log("over");
   },
   out: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
     console.log("out");
   }
   
@@ -252,6 +260,7 @@ var auditTask = function(taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+  console.log(taskEl);
 };
 
 // modal was triggered
@@ -267,7 +276,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -301,3 +310,8 @@ $("#remove-tasks").on("click", function() {
 loadTasks();
 
 
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
